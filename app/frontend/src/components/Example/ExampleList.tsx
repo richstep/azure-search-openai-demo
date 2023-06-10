32b8d896dfd1be7f0277a71c95d13f1c0adaa4f2
Richard <richgst@gmail.com>
2023-06-10 11:57:30 -0400

    Added grant language and docs

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,14 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What is the BOOT program?",
+        value: "What is the BOOT program?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    {
+        text: "Write a press release about Nextlink winning BOOT funding.",
+        value: "Write a press release about Nextlink winning BOOT funding, pointing out the importance of fixed wireless over fiber. Include a quote from the Nextlink CEO."
+    },
+    { text: "What are the BOOT funding levels?", value: "What are the BOOT funding levels?" }
 ];
 
 interface Props {
